15e9dfe82610b4026c2f3f92871cc0b2c7296e9d
Nicolas “Niols” Jeannerod <nicolas.jeannerod@tweag.io>
2024-11-04 15:45:09 +0100

    Bump Nix dependencies

--- flake.nix
+++ flake.nix
@@ -3,38 +3,45 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.nixpkgs-stable.follows = "";
     };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs =
+    inputs@{ flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
 
-      imports = [ inputs.pre-commit-hooks.flakeModule ];
+      imports = [ inputs.git-hooks.flakeModule ];
 
-      perSystem = { config, pkgs, ... }: {
-        formatter = pkgs.nixfmt;
+      perSystem =
+        { config, pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt-rfc-style;
 
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ cargo rustc git-archive-all ];
-          shellHook = config.pre-commit.installationScript;
-        };
-
-        pre-commit.settings.hooks = {
-          nixfmt = {
-            enable = true;
-            excludes = [ "vendor/.*" ];
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              cargo
+              rustc
+              git-archive-all
+            ];
+            shellHook = config.pre-commit.installationScript;
           };
-          deadnix = {
-            enable = true;
-            excludes = [ "vendor/.*" ];
+
+          pre-commit.settings.hooks = {
+            nixfmt-rfc-style = {
+              enable = true;
+              excludes = [ "vendor/.*" ];
+            };
+            deadnix = {
+              enable = true;
+              excludes = [ "vendor/.*" ];
+            };
+            opam-lint.enable = true;
           };
-          opam-lint.enable = true;
         };
-      };
     };
 }
